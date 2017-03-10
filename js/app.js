59f7761c807479b5372e33f1dc129d45dc00abc7
Monserrat Pineda <monserrat.pineda.luna@gmail.com>
2017-03-10 15:45:35 -0600

    ultima version examen sprint 1 MPL super ninja

--- js/app.js
+++ js/app.js
@@ -11,17 +11,15 @@ function agregarEstudiante() {
     // TO DO: Preguntar al usuario por el nombre, puntos técnicos y puntos de HSE de un estudiante
     // Hint: El estudiante debe ser agregado a la lista de estudiantes
     // TO DO: Retornar el estudiante recientemente creado
- var nodeEstudiantes = estudiantes.length
- for (var i = 0; i < nodeEstudiantes; i++) {
-   var nombre = prompt("Nombre de la estudiante"+ (i+1));
-   var puntosTecnicos = parseInt(prompt("¿Cuántos puntos técnicos tiene?" + (i+1)));
-   var puntosHse = parseInt(prompt("¿Cuántos puntos HSE tiene" + (i +1)));
-   var crearEstudiante = {}
+   var nombre = prompt("Nombre de la estudiante");
+   var puntosTecnicos = prompt("¿Cuántos puntos técnicos tiene?");
+   var puntosHse = prompt("¿Cuántos puntos HSE tiene");
+   var estudiante = {}
    estudiante.nombre = nombre;
    estudiante.puntosTecnicos = puntosTecnicos;
    estudiante.puntosHse = puntosHse;
-   estudiante.push(estudiantes);
-   return crearEstudiante;
+   estudiantes.push(estudiante);
+   return estudiante
 
 }
 
@@ -42,42 +40,43 @@ function mostrar(estudiante) {
     return resultado;
 }
 
-function mostrarLista(estudiantes) {
+ function mostrarLista(estudiantes) {
     // TO DO: Iterar la lista del estudiantes para devolverlos en el formato que usa la función mostrar(estudiante), NO entiendo el enunciado
     // Retornar el template de todos los estudiantes
-    var nodeEstudiantes = estudiantes.length;
-    for (var i = 0; i < nodeEstudiantes; i++);
-
-
-     return estudiantes
+   var resultado = "";
+   estudiantes.forEach(function(estudiante){
+   resultado = resultado + mostrar(estudiante);
+  });
+    return resultado;
 }
 
 function buscar(nombre, estudiantes) {
     // TO DO: Buscar el nombre en la lista de estudiantes que se recibe por parámetros
     // Retornar el objeto del estudiante buscado
     // Nota: NO IMPORTA SI EL USUARIO ESCRIBE EL NOMBRE EN MAYÚSCULAS O MINÚSCULAS
-
-    var nodeEstudiantes = estudiantes.length
-    for (var i = 0; i < nodeEstudiantes; i++);
-    var nombreabuscar = prompt("¿Qué nombre buscas?" + (i+1));
-    return nombrebuscar.toUpperCase();
-
+  var estudiantesBuscados = estudiantes.filter(function(estudiante){
+   return estudiante.nombre.tolowerCase() === estudiante.nombre.toLocaleLowerCase()
+});
+ return estudiantesBuscados
 }
+
+
+
+
 
 function topTecnico(estudiantes) {
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje técnico de mayor a menor
-    var nodeEstudiantes = estudiantes.length
-    for (var i = 0; i < nodeEstudiantes; i++);
-    var estudiantesTecnico =  estudiantes.sort(function(, ) {
-     .puntosTecnicos >  .puntosTecnicos;
-     return estudiantesTecnico[i]});
-}
+
+    var estudiantesTecnico =  estudiantes.sort(function(a,b ) {
+     a.puntosTecnicos >  b.puntosTecnicos;
+     return estudiantesTecnico;
+})
+};
 
 function topHSE(estudiantes){
     // TO DO: Retornar el arreglo de estudiantes ordenado por puntaje de HSE de mayor a menor
-    var nodeEstudiantes = estudiantes.length
-    for (var i = 0; i < nodeEstudiantes; i++);
-    var estudiantesHse = estudiantes.sort(function(c, d){
-    .puntosHse > .puntosHse;
-    return estudiantesHse[i]});
-}
+
+    var estudiantesHse = estudiantes.sort(function(a, b){
+    a.puntosHse > b.puntosHse;
+    return estudiantesHse})
+};
